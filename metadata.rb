cb85b94268c6499c0cb11dde0ef8cf1bce35eaf8
Tim Redding <tim@Timothys-MacBook-Pro.local>
2012-10-10 23:05:45 +0100

    Completed Readme.md
    And moved version to 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "chef-groovy@accio.co.uk"
 license          "All rights reserved"
 description      "Installs/Configures groovy"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.1.0"
 
 depends "java"
