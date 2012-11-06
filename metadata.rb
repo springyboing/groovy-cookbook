ba395a829c73969f0c5f9444d54b9497118e24cd
Tim Redding <tim@Timothys-MacBook-Pro.local>
2012-11-06 12:12:17 +0000

    New version number….
    0.1.1

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "chef-groovy@accio.co.uk"
 license          "All rights reserved"
 description      "Installs/Configures groovy"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 depends "java"
